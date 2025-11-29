4d1abdef8822257edb5d4d5d3e47a4f65408c3ba
jru-dev <holasoyhotaru@gmail.com>
2025-11-29 18:49:19 -0500

    swagger

--- nexus/src/main/java/com/nexus/marketplace/config/SwaggerConfig.java
+++ nexus/src/main/java/com/nexus/marketplace/config/SwaggerConfig.java
@@ -1,4 +1,39 @@
 package com.nexus.marketplace.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Nexus Marketplace API")
+                        .version("1.0.0")
+                        .description("API REST para marketplace de videojuegos")
+                        .contact(new Contact()
+                                .name("Equipo Nexus")
+                                .email("nexus@marketplace.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
+    }
 }
